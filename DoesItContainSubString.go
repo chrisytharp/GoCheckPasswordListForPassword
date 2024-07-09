d4c5d56f00abab39979e735f1b73acdb14723521
chrisytharp <37886152+chrisytharp@users.noreply.github.com>
2024-07-09 08:27:14 -0500

    Update DoesItContainSubString.go
    updated memory allocation issues

--- DoesItContainSubString.go
+++ DoesItContainSubString.go
@@ -13,9 +13,9 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
-const chunkSize = 1024 * 1024 * 50 // 50MB, experiment with this size for optimal performance
+const chunkSize = 1024 * 1024 * 5 // 5MB, experiment with this size for optimal performance
 
-func processChunk(filePath string, chunkStart, chunkSize int64, substring string, results *[]string, wg *sync.WaitGroup, progress *int64, bar *progressbar.ProgressBar, resultsMutex *sync.Mutex) {
+func processChunk(filePath string, chunkStart, chunkSize int64, substring string, results chan<- string, wg *sync.WaitGroup, progress *int64, bar *progressbar.ProgressBar) {
 	defer wg.Done()
 
 	file, err := os.Open(filePath)
@@ -30,23 +30,18 @@ func processChunk(filePath string, chunkStart, chunkSize int64, substring string
 
 	buffer := make([]byte, chunkSize)
 	n, err := reader.Read(buffer)
-	if err != nil {
+	if err != nil && err.Error() != "EOF" {
 		fmt.Printf("Failed to read file: %v\n", err)
 		return
 	}
 
 	lines := strings.Split(string(buffer[:n]), "\n")
-	var chunkResults []string
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if line != "" && strings.Contains(line, substring) {
-			chunkResults = append(chunkResults, line)
+			results <- line
 		}
 	}
-
-	resultsMutex.Lock()
-	*results = append(*results, chunkResults...)
-	resultsMutex.Unlock()
 
 	atomic.AddInt64(progress, chunkSize)
 	bar.Add64(chunkSize)
@@ -90,10 +85,8 @@ func main() {
 	substring := os.Args[1]
 	passwordListFile := os.Args[2]
 
-	var results []string
 	progress := int64(0)
 	var wg sync.WaitGroup
-	var resultsMutex sync.Mutex
 
 	chunks, err := chunkify(passwordListFile, chunkSize)
 	if err != nil {
@@ -117,26 +110,30 @@ func main() {
 		progressbar.OptionFullWidth(),
 	)
 
-	numWorkers := runtime.NumCPU() * 2 // Adjust the number of workers based on CPU cores
+	numWorkers := runtime.NumCPU() // Adjust the number of workers based on CPU cores
 	sem := make(chan struct{}, numWorkers)
+	results := make(chan string, 1000) // Buffered channel to collect results
+
+	go func() {
+		for result := range results {
+			fmt.Println(result)
+		}
+	}()
 
 	for _, chunk := range chunks {
 		wg.Add(1)
 		sem <- struct{}{}
 		go func(chunkStart, chunkSize int64) {
 			defer func() { <-sem }()
-			processChunk(passwordListFile, chunkStart, chunkSize, substring, &results, &wg, &progress, bar, &resultsMutex)
+			processChunk(passwordListFile, chunkStart, chunkSize, substring, results, &wg, &progress, bar)
 		}(chunk[0], chunk[1])
 	}
 
 	wg.Wait()
+	close(results)
 	bar.Finish()
 
 	duration := time.Since(startTime)
 	fmt.Printf("Total time taken: %v\n", duration)
-
-	fmt.Printf("Passwords containing '%s':\n", substring)
-	for _, result := range results {
-		fmt.Println(result)
-	}
 }
+
